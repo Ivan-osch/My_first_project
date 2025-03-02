9f437239c9bb38b9323f26306b44cd87323ddda6
Ivan-osch <vanes-oschepkov@mail.ru>
2025-03-02 21:44:08 +0700

    Проверка нескольких промо. Добавлена параметризация теста

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,12 @@
-import time
+import pytest
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('promo', ["0", "1", "2", "3", "4", "5", "6",
+                                   pytest.param("7", marks=pytest.mark.xfail),
+                                   "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}"
     page = ProductPage(browser, link)
     page.open()
     product_name = page.product_name()  # получаем имя товара на странице

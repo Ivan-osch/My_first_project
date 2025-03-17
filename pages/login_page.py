8c6c1f3dba823f3a4626a9b5eab007dc075a9d87
Ivan-osch <vanes-oschepkov@mail.ru>
2025-03-17 23:25:03 +0700

    Добавлен метод register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -19,3 +19,9 @@ class LoginPage(BasePage):
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL), "Register email is not presented"
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD1), "Register password1 is not presented"
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD2), "Register password2 is not presented"
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD1).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_BUTTON).click()

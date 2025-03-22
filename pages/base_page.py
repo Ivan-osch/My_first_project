44539b3c943da76209a13bd884524196e2876d05
Ivan-osch <vanes-oschepkov@mail.ru>
2025-03-22 17:33:11 +0700

    методы выставлены в алфавитном порядке

--- pages/base_page.py
+++ pages/base_page.py
@@ -16,19 +16,12 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
-        link.click()
-
-    def should_be_authorized_user(self):
-        assert self.is_element_present(*BasePageLocators.USER_ICON), \
-            "User icon is not presented, probably unauthorised user"
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
-
     def go_to_basket_page_header(self):
         link = self.browser.find_element(*BasePageLocators.BASKET_HEADER)
+        link.click()
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
     def is_disappeared(self, how, what, timeout=4):
@@ -57,6 +50,13 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), \
+            "User icon is not presented, probably unauthorised user"
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert

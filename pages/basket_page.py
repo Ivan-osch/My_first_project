dbc1b2e69a5b7368901639f2e5a60cb834a6de8f
Ivan-osch <vanes-oschepkov@mail.ru>
2025-03-22 17:33:21 +0700

    методы выставлены в алфавитном порядке

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -3,9 +3,9 @@ from .locators import BasketPageLocators
 
 
 class BasketPage(BasePage):
+    def should_be_basket_empty(self):
+        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY), "No message that the basket is empty"
+
     def should_not_be_items_in_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), \
             "Items in basket, but should not be"
-
-    def should_be_basket_empty(self):
-        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY), "No message that the basket is empty"

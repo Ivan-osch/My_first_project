39b9d2289b3759ac0fd16dffe8b9c4553c783497
Ivan-osch <vanes-oschepkov@mail.ru>
2025-03-22 17:33:29 +0700

    методы выставлены в алфавитном порядке

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,10 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_USERNAME), "Login username is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Login password is not presented"
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -10,10 +14,6 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         assert "login" in self.browser.current_url, "Invalid URL"
-
-    def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_USERNAME), "Login username is not presented"
-        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Login password is not presented"
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL), "Register email is not presented"
